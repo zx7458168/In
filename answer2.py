7454c40bd6b4c97cd10b4307f545c1c29f77aed3
zx7458168 <45328984+zx7458168@users.noreply.github.com>
2021-01-24 14:36:48 +0800

    Update answer2.py

--- answer2.py
+++ answer2.py
@@ -1,21 +1,48 @@
-import time
 import math
+import time
+import json
 
-class Data:
-    current_time = time.time()
-    last_access_time = float(open('saveTime.txt', 'r').readline())
-    list = {}
-    score = 0
+maxSize = 20
+list = {}
+
+def saveToJson():
+    data2 = json.dumps(list)
+    f = open("json.txt", 'w')
+    f.write(data2)
+    f.close()
 
 def get(key):
-    return Data.list[key] if key in list else -1
+    if (key in list):
+        # refresh last access time
+        list[key][2] = time.time()
+        saveToJson()
+        return list[key]
+    else:
+        return -1
 
 def put(key, value, weight=0):
-    Data.list[key] = value
-    f = open('saveTime.txt','w')
-    f.write(str(Data.current_time))
-    f.close()
-    Data.score = scoreProducer(weight)
+    # if maxsize is reached, replace it
+    key = str(key)
+    if (len(list) >= maxSize):
+        lowestKey = findLowestItem()
+        del list[lowestKey]
+        list[key] = [value, weight, time.time()]
+    else:
+        list[key] = [value, weight, time.time()]
+    saveToJson()
 
-def scoreProducer(weight):
-    return weight/math.log(Data.current_time - Data.last_access_time) if Data.last_access_time != Data.current_time else weight/-100
+# find item with lowest value, return the key of that item
+def findLowestItem():
+    # get first key and item
+    key = next(iter(list))
+    low = getScore(list[key][1], list[key][2])
+    # find item with lowest value
+    for k, v in list.items():
+        score = getScore(v[1], v[2])
+        if low > score:
+            low = score
+            key = k
+    return key
+
+def getScore(w, lastAccessTime):
+    return w / math.log(int(time.time()*1000) - int(lastAccessTime*1000)) if lastAccessTime != time.time() else w / -100
